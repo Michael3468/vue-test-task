212fe375a88e7e225effefda63ca5cb6ccfb7d30
Mikhail <mikhail3468@yandex.ru>
2023-08-07 16:15:28 +0300

    feat(dummyview): add styles to center text

--- src/views/DummyView.vue
+++ src/views/DummyView.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>Dummy component</h1>
+  <div class="dummy-view">
+    <h1>Dummy component</h1>
+  </div>
 </template>
+
+<style>
+.dummy-view {
+  display: flex;
+  justify-content: center;
+}
+</style>
